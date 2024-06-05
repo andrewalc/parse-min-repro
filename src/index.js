d700e002fb33199d7eff516c0e973548c36161fe
Andrew Alcala <andrewalc98@gmail.com>
2024-06-05 18:39:05 -0400

    minimal repro

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
 
-const app = express();
-
 async function main() {
-  const server = new ParseServer({
+  const app = express();
+  const config = {
     databaseURI: process.env.DATABASE_URI, // Connection string for your MongoDB database
-    cloud: cloudCode, // Path to your Cloud Code
-    appId: 'myAppId',
-    masterKey: 'myMasterKey', // Keep this key secret!
-    fileKey: 'optionalFileKey',
-    serverURL: 'http://localhost:1337/parse', // Don't forget to change to https if needed
-    masterKeyIps: ['0.0.0.0/0', '::/0'],
-  });
-
+    cloud: () => { }, // Path to your Cloud Code
+    appId: process.env.APP_ID,
+    masterKey: process.env.MASTER_KEY, // Keep this key secret!
+    serverURL: process.env.SERVER_URL, // Don't forget to change to https if needed
+    masterKeyIps: ['0.0.0.0/0', '::/0'], // Any IP allowed for dashboard access
+  };
   // Start server
+  const server = new ParseServer(config);
   await server.start();
 
   // Serve the Parse API on the /parse URL prefix
@@ -22,15 +20,17 @@ async function main() {
 
   app.listen(1337, function () {
     console.log('parse-server-example running on port 1337.');
+    // Once parse is set up run your code
+    afterStart();
   });
 }
 
-function cloudCode() {
-  const obj = new Parse.Object("TestObject");
-
-  console.log("Cloud code complete");
+async function afterStart() {
+  // Put parse code here
+  console.log("Code complete");
 }
 
+// Start the server
 main().then(() => {
   console.log('Server startup complete');
 });
